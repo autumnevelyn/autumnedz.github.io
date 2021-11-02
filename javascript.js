50d5a94ca8cf8b20c2dfa580cc2c777e205ce621
autumnedz <68071174+autumnedz@users.noreply.github.com>
2021-11-02 20:44:41 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -81,5 +81,7 @@ const links = [
 
 const getRandomLink = () =>{
   const randomIndex = Math.floor(Math.random() * links.length)
+  console.log('index: ', rabdomIndex)
+  console.log('link: ', links[randomIndex])
   document.body.innerHTML = links[randomIndex]
 }
